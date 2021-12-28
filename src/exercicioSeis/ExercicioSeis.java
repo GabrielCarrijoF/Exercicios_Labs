2d6e6071601243b038e5a97752955b4182f93d93
Gabriel Ferreira Carrijo <gabriel.carrijo@luizalabs.com>
2021-12-28 10:56:00 -0300

    Exercicio Seis

--- src/exercicioSeis/ExercicioSeis.java
+++ src/exercicioSeis/ExercicioSeis.java
@@ -10,26 +10,38 @@ public class ExercicioSeis {
     Locale.setDefault(Locale.US);
     Scanner scanner = new Scanner(System.in);
 
-    int valorA, valorB, valorC, soma;
+    int valorA, valorB, valorC, soma = 0;
 
-    System.out.println("Inforeme um valor");
+    System.out.println("Inforeme valor1");
     valorA = scanner.nextInt();
 
-    System.out.println("Inforeme outro valor");
+    System.out.println("Inforeme valor2");
     valorB = scanner.nextInt();
 
-    System.out.println("Inforeme outro valor");
+    System.out.println("Inforeme valor3");
     valorC = scanner.nextInt();
 
-    if (valorA > valorB & valorB < valorC) {
-      soma = valorA + valorC;
+    if (valorA > valorB) {
+      if (valorB > valorC) {
+        soma = valorA + valorB;
+      } else {
+        soma = valorA + valorC;
+      }
+    } else if (valorB > valorA) {
+      if (valorC > valorA) {
+        soma = valorB + valorC;
+      } else {
+        soma = valorA + valorB;
+      }
+    } else if (valorC > valorA) {
+      if (valorA > valorB) {
+        soma = valorC + valorA;
+      } else {
+        soma = valorC + valorB;
+      }
     }
-    if (valorB > valorA & valorB < valorC) {
-      soma = valorB + valorC;
-    } else {
-      soma = valorC + valorB;
-    }
-
+    System.out.print(soma);
     scanner.close();
   }
 }
+
