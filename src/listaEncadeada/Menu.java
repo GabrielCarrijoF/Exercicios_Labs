dcee4123e577cc5847a88a68517d0cfd77d69079
Gabriel Ferreira Carrijo <gabriel.carrijo@luizalabs.com>
2022-01-06 23:13:55 -0300

    ExercicioDez lista encadeada

---
+++ src/listaEncadeada/Menu.java
@@ -0,0 +1,49 @@
+package listaEncadeada;
+
+
+import java.util.Scanner;
+import listaEncadeada.node.List;
+
+public class Menu {
+
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+
+    List list = new List();
+    
+    int opicao;
+
+    do {
+      showMenu();
+      opicao = sc.nextInt();
+
+      switch (opicao) {
+        case 1: {
+          System.out.println("Digite um numero");
+          double value = sc.nextDouble();
+          list.add(value);
+          break;
+        }
+        case 2: {
+          System.out.println(list.toString());
+          break;
+        }
+        case 3: {
+          System.out.println("Fim do Programa");
+          break;
+        }
+        default:
+          System.out.println("Opção invalida");
+      }
+    } while (opicao != 3);
+
+    sc.close();
+  }
+
+  public static void showMenu() {
+    System.out.println("1- Inserir elemento na tabela");
+    System.out.println("2- Mostrar lista");
+    System.out.println("3- Sair");
+  }
+}
---
+++ src/listaEncadeada/node/List.java
@@ -0,0 +1,28 @@
+package listaEncadeada.node;
+
+public class List {
+
+  private Node head;
+
+  public void add(Double value) {
+    Node node = new Node();
+    node.setValue(value);
+    node.setNext(head);
+    head = node;
+  }
+
+  @Override
+  public String toString() {
+    StringBuffer stringBuffer = new StringBuffer();
+    stringBuffer.append("[");
+
+    Node p = head;
+    while (p != null) {
+      stringBuffer.append(p.getValue() + " ");
+      p = p.getNext();
+    }
+
+    stringBuffer.append("]");
+    return stringBuffer.toString();
+  }
+}
